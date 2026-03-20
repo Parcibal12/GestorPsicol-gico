2aa91dd8eb6c75b0e9fdc31754b5ccebcb0294f3
Parcibal12 <danielirigoyen984@gmail.com>
2026-03-20 17:29:58 -0400

    feat(validacion): implementando logica de validacion limpia y mocks de pacientes para HU-06

--- blocks/registro-turno/registro-turno.js
+++ blocks/registro-turno/registro-turno.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {    
     
     flatpickr("#input-fecha", {
         locale: "es",
@@ -23,4 +23,24 @@ document.addEventListener('DOMContentLoaded', () => {
         allowInput: true
     });
 
+    
+    const formularioCita = document.getElementById('formulario-cita');
+
+    formularioCita.addEventListener('submit', function(evento) {
+        
+        evento.preventDefault();
+
+        const pacienteId = document.getElementById('select-paciente').value;
+        const fecha = document.getElementById('input-fecha').value;
+        const horaInicio = document.getElementById('input-hora-inicio').value;
+        const horaFin = document.getElementById('input-hora-fin').value;
+
+        if (pacienteId === '' || fecha === '' || horaInicio === '' || horaFin === '') {
+            alert('Completa todos los campos obligatorios (*)');
+            return;
+        }
+
+        alert('Turno validado correctamente');        
+        formularioCita.reset();
+    });
 });
