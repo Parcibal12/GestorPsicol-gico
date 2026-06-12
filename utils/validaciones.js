9c03836d4c86df1b9428251f61bf686ed508797b
Parcibal12 <danielirigoyen984@gmail.com>
2026-06-12 19:47:34 -0400

    feat: [HU-06] implementando validación de horas en validarCamposTurno para pasar el test

--- utils/validaciones.js
+++ utils/validaciones.js
@@ -2,5 +2,10 @@ export function validarCamposTurno(pacienteId, fecha, horaInicio, horaFin) {
     if (pacienteId === '' || fecha === '' || horaInicio === '' || horaFin === '') {
         return false;
     }
+    
+    if (horaInicio >= horaFin) {
+        return false;
+    }
+
     return true;
 }
