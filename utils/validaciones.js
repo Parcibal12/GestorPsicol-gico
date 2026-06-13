ccb47c6d85e15ba26c23e380e13669551a73a88c
Parcibal12 <danielirigoyen984@gmail.com>
2026-06-12 20:17:12 -0400

    refactor: [HU-11] eliminando magic numbers extrayendo constantes de tiempo

--- utils/validaciones.js
+++ utils/validaciones.js
@@ -7,3 +7,12 @@ export function validarCamposTurno(pacienteId, fecha, horaInicio, horaFin) {
 
     return true;
 }
+
+export function esNotaEditable(fechaCreacion, fechaActual) {
+    const LIMITE_HORAS_EDICION = 24;
+    const MILISEGUNDOS_POR_HORA = 1000 * 60 * 60;
+    
+    const diferenciaHoras = (fechaActual - fechaCreacion) / MILISEGUNDOS_POR_HORA;
+    
+    return diferenciaHoras <= LIMITE_HORAS_EDICION;
+}
